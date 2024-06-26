bd0eea095930997e0d38c51f3d86d7107e146206
Owaiz13 <mohowaissheikh@gamil.com>
2024-06-26 20:19:32 +0530

    route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,32 @@
-// src/App.js
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
 import FileUpload from './FileUpload';
 
 function App() {
   return (
-    <div className="App">
-      <nav className="navbar bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Decompiler</a>
+    <Router>
+      <div className="App">
+        <nav className="navbar bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">Decompiler</a>
+          </div>
+        </nav>
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/decompiler" element={<FileUpload />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
         </div>
-      </nav>
-      <div className="container mt-4">
-        <FileUpload />
       </div>
-    </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <h1>Welcome to Decompiler!</h1>
+    // Additional components or content here
   );
 }
 
